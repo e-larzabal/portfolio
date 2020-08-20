5106eb70b260e8df8928d089915d15d55a2f639f
Emmanuelle LARZABAL <emmanuelle.larzabal64@gmail.com>
2020-08-20 15:12:17 +0200

    create and test API for contact model

--- src/main/java/com/wcs/checkpoint2/portfolio/controller/ContactAPIController.java
+++ src/main/java/com/wcs/checkpoint2/portfolio/controller/ContactAPIController.java
@@ -21,45 +21,46 @@ public class ContactAPIController {
     private ContactService contactService;
 
 
-//    // CRUD : C
-//    @PostMapping("/api/contacts")
-//    @ResponseBody
-//    public Contact create(@RequestBody Contact contact){
-//        return contactService.create(contact);
-//    }
-//
-//    // CRUD : R
-//    @GetMapping("/api/contacts/{uuid}")
-//    @ResponseBody
-//    public Optional<Contact> get(@PathVariable UUID uuid){
-//        return contactService.find(uuid);
-//    }
-//
-//    // CRUD : R
-//    @GetMapping("/api/contacts")
-//    @ResponseBody
-//    public List<Contact> list(){
-//        return contactService.list();
-//    }
-//
-//    // CRUD : U
-//    @PutMapping("/api/contacts")
-//    @ResponseBody
-//    public Contact update(@RequestBody Contact contact) {
-//        //if (player.getUuid() == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing uuid");
-//        //else return service.update(player);
-//        return contactService.update(contact);
-//    }
-//
-//    // CRUD : D
-//    @GetMapping("/api/contacts/{uuid}")
-//    @ResponseBody
-//    public ResponseEntity<Void> delete(@PathVariable UUID uuid){
-//        boolean isDeleted = contactService.delete(uuid);
-//        if (isDeleted) {
-//            return null;
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "could not delete player with uuid> " + uuid);
-//        }
-//    }
+    // CRUD : C
+    @PostMapping("/api/contact")
+    @ResponseBody
+    public Contact create(@RequestBody Contact contact){
+        return contactService.create(contact);
+    }
+
+    // CRUD : R
+    @GetMapping("/api/contact/{uuid}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public Optional<Contact> get(@PathVariable UUID uuid){
+       return contactService.find(uuid);
+    }
+
+    // CRUD : R
+    @GetMapping("/api/contacts")
+    @ResponseBody
+    public List<Contact> list(){
+        return contactService.list();
+    }
+
+    // CRUD : U
+    @PutMapping("/api/contact")
+    @ResponseBody
+    public Contact update(@RequestBody Contact contact) {
+       if (contact.getUuid() == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing uuid");
+       else return contactService.update(contact);
+    }
+
+    // CRUD : D
+    @DeleteMapping("/api/contact/{uuid}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> delete(@PathVariable UUID uuid){
+        boolean isDeleted = contactService.delete(uuid);
+        if (isDeleted) {
+            return null;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "could not delete contact with uuid> " + uuid);
+        }
+    }
 }
